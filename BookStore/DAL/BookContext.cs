68494cef3811b617f17bc07865f44508ce6ebcfb
SimonTong <simontong@simon.com>
2016-02-23 17:14:36 +0800

    TestCommit

---
+++ BookStore/DAL/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using BookStore.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BookStore.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext()
+            : base("BookContext")
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
